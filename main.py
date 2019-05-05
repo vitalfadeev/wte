998423382a2e7d7d106860a0f62822f97e05becd
Vital Fadeev <vital.fadeev@gmail.com>
2019-05-05 14:16:57 +0700

    Add logging

--- main.py
+++ main.py
@@ -24,24 +24,28 @@ if __name__ == "__main__":
     # Save
     # save to json
     json_filename = os.path.join(".", 'result.json')
+    wte.log.info("Saving to the %s", json_filename )
     treemap.save_to_json(json_filename)
 
     # save to pickle
     pickle_filename = os.path.join(".", "result.pickled")
+    wte.log.info("Saving to the %s", pickle_filename )
     treemap.save_to_pickle(pickle_filename)
     
     # Laod
     # laod json
     treemap = wte.TreeMap()
+    wte.log.info("Loading from the %s", json_filename )
     treemap.load_from_json(json_filename)
     
     # laod pickle
     treemap = wte.TreeMap()
+    wte.log.info("Loading from the %s", pickle_filename )
     treemap.load_from_pickle(pickle_filename)
     
     # Show
     # show words
+    wte.log.info("Printing words: %d words", len(treemap.store))
     for label, words in treemap.store.items():
         print(label.ljust(40), words)
-    
     
